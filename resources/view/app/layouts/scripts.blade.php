81303f4cfcd36c3aac1dc714e5ab8cea28115b3f
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2021-12-31 22:48:38 +0330

    add all js file need to show application

--- resources/view/app/layouts/scripts.blade.php
+++ resources/view/app/layouts/scripts.blade.php
@@ -1,27 +1,10 @@
-<!-- jquery-->
-<script src="<?= asset('js/jquery-2.2.4.min.js') ?>" type="text/javascript"></script>
-<!-- Plugins js -->
-<script src="<?= asset('js/plugins.js') ?>" type="text/javascript"></script>
-<!-- Popper js -->
-<script src="<?= asset('js/popper.js') ?>" type="text/javascript"></script>
-<!-- Bootstrap js -->
-<script src="<?= asset('js/bootstrap.min.js') ?>" type="text/javascript"></script>
-<!-- WOW JS -->
-<script src="<?= asset('js/wow.min.js') ?>"></script>
-<!-- Owl Cauosel JS -->
-<script src="<?= asset('vendor/OwlCarousel/owl.carousel.min.js') ?>" type="text/javascript"></script>
-<!-- Meanmenu Js -->
-<script src="<?= asset('js/jquery.meanmenu.min.js') ?>" type="text/javascript"></script>
-<!-- Srollup js -->
-<script src="<?= asset('js/jquery.scrollUp.min.js') ?>" type="text/javascript"></script>
-<!-- jquery.counterup js -->
-<script src="<?= asset('js/jquery.counterup.min.js') ?>"></script>
-<script src="<?= asset('js/waypoints.min.js') ?>"></script>
-<!-- Isotope js -->
-<script src="<?= asset('js/isotope.pkgd.min.js') ?>" type="text/javascript"></script>
-<!-- Magnific Popup -->
-<script src="<?= asset('js/jquery.magnific-popup.min.js') ?>"></script>
-<!-- Ticker Js -->
-<script src="<?= asset('js/ticker.js') ?>" type="text/javascript"></script>
-<!-- Custom Js -->
-<script src="<?= asset('js/main.js') ?>" type="text/javascript"></script>
+    <!-- jQuery Scripts -->
+    <script src="<?= asset('js/jquery.min.js') ?>"></script>
+    <script src="<?= asset('js/bootstrap.min.js') ?>"></script>
+    <script src="<?= asset('js/easing.min.js') ?>"></script>
+    <script src="<?= asset('js/owl-carousel.min.js') ?>"></script>
+    <script src="<?= asset('js/flickity.pkgd.min.js') ?>"></script>
+    <script src="<?= asset('js/twitterFetcher_min.js') ?>"></script>
+    <script src="<?= asset('js/jquery.newsTicker.min.js') ?>"></script>
+    <script src="<?= asset('js/modernizr.min.js') ?>"></script>
+    <script src="<?= asset('js/scripts.js') ?>"></script>
