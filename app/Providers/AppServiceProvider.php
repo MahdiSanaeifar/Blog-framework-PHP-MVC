8f9b24aefb01963e9b830d2255bd00233ef25ebc
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2022-01-01 22:55:58 +0330

    add appServiceProvider to send mostPopularPost variable in ['app.category', 'app.post', 'app.search', 'app.all-post'] routes

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use App\Category;
+use App\Post;
+use System\View\Composer;
+
+class AppServiceProvider extends Provider
+{
+    /**
+     * Bootstrap the application services.
+     */
+
+    public function boot()
+    {
+        Composer::view(['app.category', 'app.post', 'app.search', 'app.all-post'], function () {
+            // $categories = Category::whereNull('parent_id')->orderBy('created_at', 'desc')->limit(0, 6)->get();
+            $mostPopularPost = Post::where('published_at', '<=', date('Y-m-d H:i:s'))->orderBy('views', 'desc')->limit(0, 4)->get();
+            return [
+                "mostPopularPost" => $mostPopularPost
+            ];
+        });
+    }
+}
