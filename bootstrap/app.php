2b430fb733b8128c348a88f37d69a13c34b34e23
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2022-01-13 11:49:56 +0330

    add bootstrap to run application

---
+++ bootstrap/app.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is to run Application class, Application loads
+| Providers, Helpers, Routes and project Routing system; then project starting.
+|
+*/
+
+new \System\Application\Application();
