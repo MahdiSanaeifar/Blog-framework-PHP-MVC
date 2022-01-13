ec5212300e5cb13543385af8c4bcf0e188503fa1
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2022-01-13 10:07:57 +0330

    edit email configuration

--- config/mail.php
+++ config/mail.php
@@ -5,7 +5,7 @@ return [
         'Host'       => 'smtp.gmail.com',
         'SMTPAuth'   => true,
         'Username'   => 'mahdisanaeifar2@gmail.com',
-        'Password'   => '09398565492',
+        'Password'   => 'myPass',
         'Port'       => 587,
         'setFrom'    => [
             'mail'  =>  'mahdisanaeifar2@gmail.com',
