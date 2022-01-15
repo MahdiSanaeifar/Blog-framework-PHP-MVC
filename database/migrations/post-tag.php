dcab4c5fc94a43db5dc6a40348935175dfacac84
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2022-01-15 13:04:01 +0330

    create posts-tags table sql

--- database/migrations/post-tag.php
+++ database/migrations/post-tag.php
@@ -2,8 +2,11 @@
 
 return [
   "CREATE TABLE `posts-tags` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `post_id` bigint(20)  NOT NULL,
   `tag_id` bigint(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
   FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
   FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;"
