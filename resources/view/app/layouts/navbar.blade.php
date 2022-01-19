bbe7a5e5800626acddd7d39620d6a079b452bbe7
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2022-01-19 19:36:39 +0330

    rewrite social media linkes and edit manu in mobile size

--- resources/view/app/layouts/navbar.blade.php
+++ resources/view/app/layouts/navbar.blade.php
@@ -17,46 +17,63 @@
                     <a href="<?=route('home.index')?>" class="sidenav__menu-url">صفحه اصلی</a>
                 </li>
                 <li>
-                    <a href="#" class="sidenav__menu-url">صفحات</a>
+                    <a href="<?=route('home.all.post')?>" class="sidenav__menu-url">تمامی اخبار</a>
+                </li>
+                <li>
+                    <a href="#" class="sidenav__menu-url">دسته ها</a>
                     <button class="sidenav__menu-toggle" aria-haspopup="true" aria-label="Open dropdown"><i
-                            class="ui-arrow-down"></i></button>
+                                class="ui-arrow-down"></i></button>
                     <ul class="sidenav__menu-dropdown">
-                        <li><a href="<?=route('home.about')?>" class="sidenav__menu-url">درباره ما</a></li>
-                        <li><a href="<?=route('home.contact')?>" class="sidenav__menu-url">تماس با ما</a></li>
-                        <li><a href="search-results.html" class="sidenav__menu-url">نتایج جستجو</a></li>
-                        <li><a href="categories.html" class="sidenav__menu-url">دسته بندی مطالب</a></li>
-                        <li><a href="single-post.html" class="sidenav__menu-url">نمایش مطلب</a></li>
-                        <li><a href="404.html" class="sidenav__menu-url">404</a></li>
+                        <li><a href="<?=route('home.category', [2])?>" class="sidenav__menu-url">بین الملل</a></li>
+                        <li><a href="<?=route('home.category', [3])?>" class="sidenav__menu-url">ورزشی</a></li>
+                        <li><a href="<?=route('home.category', [4])?>" class="sidenav__menu-url">سیاسی</a></li>
+                        <li><a href="<?=route('home.category', [6])?>" class="sidenav__menu-url">فناوری</a></li>
+                        <li><a href="<?=route('home.category', [7])?>" class="sidenav__menu-url">خودرو</a></li>
                     </ul>
                 </li>
 
                 <!-- Categories -->
                 <li>
-                    <a href="#" class="sidenav__menu-url">تکنولوژی</a>
+                    <a href="<?=route('home.about')?>" class="sidenav__menu-url">درباره ما</a>
                 </li>
                 <li>
-                    <a href="#" class="sidenav__menu-url">زیبایی و سلامت</a>
+                    <a href="<?=route('home.contact')?>" class="sidenav__menu-url">تماس با ما</a>
                 </li>
-                <li>
-                    <a href="#" class="sidenav__menu-url">موبایل</a>
-                </li>
+
+                <?php if (\System\Auth\Auth::checkLogin()) {?>
+                <?php if (\System\Auth\Auth::user()->user_type == 'admin') {?>
+                <li><a href="<?=route('admin.index')?>" class="sidenav__menu-url">پنل ادمین</a></li>
+                <?php }?>
+                <li><a href="<?=route('home.index')?>"
+                       class="sidenav__menu-url"><?=\System\Auth\Auth::user()->username?></a></li>
+                <li><a href="<?=route('auth.logout')?>" class="sidenav__menu-url">خروج</a></li>
+                <?php } else {?>
+                <li><a href="<?=route('auth.login.view')?>" class="sidenav__menu-url">ورود</a></li>
+                <li><a href="<?=route('auth.register.view')?>" class="sidenav__menu-url">ثبت نام</a></li>
+                <?php }?>
+
             </ul>
         </nav>
 
         <div class="socials sidenav__socials">
-            <a class="social social-facebook" href="#" target="_blank" aria-label="facebook">
+            <a class="social social-facebook"
+               href="<?= App\System::where('section', '=', 'facebook')->get()[0]->content ?>" target="_blank"
+               aria-label="facebook">
                 <i class="ui-facebook"></i>
             </a>
-            <a class="social social-twitter" href="#" target="_blank" aria-label="twitter">
+            <a class="social social-twitter"
+               href="<?= App\System::where('section', '=', 'twitter')->get()[0]->content ?>" target="_blank"
+               aria-label="twitter">
                 <i class="ui-twitter"></i>
             </a>
-            <a class="social social-google-plus" href="#" target="_blank" aria-label="google">
+            <a class="social social-google-plus"
+               href="<?= App\System::where('section', '=', 'googleplus')->get()[0]->content ?>" target="_blank"
+               aria-label="google">
                 <i class="ui-google"></i>
             </a>
-            <a class="social social-youtube" href="#" target="_blank" aria-label="youtube">
-                <i class="ui-youtube"></i>
-            </a>
-            <a class="social social-instagram" href="#" target="_blank" aria-label="instagram">
+            <a class="social social-instagram"
+               href="<?= App\System::where('section', '=', 'instagram')->get()[0]->content ?>" target="_blank"
+               aria-label="instagram">
                 <i class="ui-instagram"></i>
             </a>
         </div>
@@ -75,13 +92,13 @@
 
                         <?php if (\System\Auth\Auth::checkLogin()) {?>
                         <?php if (\System\Auth\Auth::user()->user_type == 'admin') {?>
-                            <li><a href="<?=route('admin.index')?>">پنل ادمین</a></li>
+                        <li><a href="<?=route('admin.index')?>">پنل ادمین</a></li>
                         <?php }?>
                         <li><a href="<?=route('home.index')?>"><?=\System\Auth\Auth::user()->username?></a></li>
                         <li><a href="<?=route('auth.logout')?>">خروج</a></li>
                         <?php } else {?>
-                            <li><a href="<?=route('auth.login.view')?>">ورود</a></li>
-                            <li><a href="<?=route('auth.register.view')?>">ثبت نام</a></li>
+                        <li><a href="<?=route('auth.login.view')?>">ورود</a></li>
+                        <li><a href="<?=route('auth.register.view')?>">ثبت نام</a></li>
                         <?php }?>
 
                     </ul>
@@ -90,19 +107,24 @@
                 <!-- Socials -->
                 <div class="col-lg-6">
                     <div class="socials nav__socials socials--nobase socials--white justify-content-start">
-                        <a class="social social-facebook" href="#" target="_blank" aria-label="facebook">
+                        <a class="social social-facebook"
+                           href="<?= App\System::where('section', '=', 'facebook')->get()[0]->content ?>"
+                           target="_blank" aria-label="facebook">
                             <i class="ui-facebook"></i>
                         </a>
-                        <a class="social social-twitter" href="#" target="_blank" aria-label="twitter">
+                        <a class="social social-twitter"
+                           href="<?= App\System::where('section', '=', 'twitter')->get()[0]->content ?>" target="_blank"
+                           aria-label="twitter">
                             <i class="ui-twitter"></i>
                         </a>
-                        <a class="social social-google-plus" href="#" target="_blank" aria-label="google">
+                        <a class="social social-google-plus"
+                           href="<?= App\System::where('section', '=', 'googleplus')->get()[0]->content ?>"
+                           target="_blank" aria-label="google">
                             <i class="ui-google"></i>
                         </a>
-                        <a class="social social-youtube" href="#" target="_blank" aria-label="youtube">
-                            <i class="ui-youtube"></i>
-                        </a>
-                        <a class="social social-instagram" href="#" target="_blank" aria-label="instagram">
+                        <a class="social social-instagram"
+                           href="<?= App\System::where('section', '=', 'instagram')->get()[0]->content ?>"
+                           target="_blank" aria-label="instagram">
                             <i class="ui-instagram"></i>
                         </a>
                     </div>
@@ -121,7 +143,7 @@
 
                     <!-- Side Menu Button -->
                     <button class="nav-icon-toggle d-xl-none d-lg-none" id="nav-icon-toggle"
-                        aria-label="Open side menu">
+                            aria-label="Open side menu">
                         <span class="nav-icon-toggle__box">
                             <span class="nav-icon-toggle__inner"></span>
                         </span>
@@ -179,7 +201,7 @@
                             <div class="nav__search-box" id="nav__search-box">
                                 <form class="nav__search-form" action="<?=route('home.search')?>" method="GET">
                                     <input type="text" placeholder="جستجو مقالات" class="nav__search-input"
-                                        name="search">
+                                           name="search">
                                     <button type="submit" class="search-button btn btn-lg btn-color btn-button">
                                         <i class="ui-search nav__search-icon"></i>
                                     </button>
